ef569754992799d334d9431d865b56e679f14b0f
123trung123 <ngoctrungtran2904@gmail.com>
2024-08-15 22:52:37 +0700

    upload1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Exam from './components/Exam'
+import Exam2 from './components/Exam2'
+import ToToList from "./components/ToToList"
+export default function App() {
+  return (
+    <div>
+      <Exam/>
+      <Exam2/>
+      <ToToList/>
+    </div>
+  )
+}
